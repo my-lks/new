2caf9421278a4946a56cd805002500f03dd6d22a
Kaoshan Li <Kaoshan_Li@asus.com>
2018-09-27 16:15:52 +0800

    add

---
+++ snarf-7.0/config.h
@@ -0,0 +1,57 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define to `long' if <sys/types.h> doesn't define.  */
+/* #undef off_t */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+#define PACKAGE "snarf"
+#define VERSION "7.0"
+#define PROTOTYPES 1
+/* #undef socklen_t */
+/* #undef ssize_t */
+
+/* Use ioctl() to determine window width */
+/* #undef GUESS_WINSIZE */
+
+/* Don't print progress bars by default */
+/* #undef PROGRESS_DEFAULT_OFF */
+
+/* Define whether to build in SOCKS 5 support */
+/* #undef USE_SOCKS5 */
+
+/* Define if you have the gettimeofday function.  */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define if you have the strdup function.  */
+#define HAVE_STRDUP 1
+
+/* Define if you have the strerror function.  */
+#define HAVE_STRERROR 1
+
+/* Define if you have the <stdarg.h> header file.  */
+#define HAVE_STDARG_H 1
+
+/* Define if you have the <string.h> header file.  */
+/* #undef HAVE_STRING_H */
+
+/* Define if you have the <strings.h> header file.  */
+#define HAVE_STRINGS_H 1
+
+/* Define if you have the <sys/ioctl.h> header file.  */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Name of package */
+#define PACKAGE "snarf"
+
+/* Version number of package */
+#define VERSION "7.0"
+
+/* Define if compiler has function prototypes */
+#define PROTOTYPES 1
+
